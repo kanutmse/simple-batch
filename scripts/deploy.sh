22fb96c97cde1144fac68aed6aefeda9249be5ac
kanutm <kanutm@NongAct.local>
2022-03-10 13:14:43 +0700

    update helm & deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-docker build -t asia.gcr.io/poc-innovation-iot/simple-cronjob:uat .
-docker push asia.gcr.io/poc-innovation-iot/simple-cronjob:uat
+docker build -t asia.gcr.io/poc-innovation-iot/simple-cronjob:v1.0 ..
+docker push asia.gcr.io/poc-innovation-iot/simple-cronjob:v1.0
 
 gcloud container clusters get-credentials poc-cluster-cmmn --zone asia-southeast1-b --project poc-innovation-iot
-helm upgrade --install simple-cronjob --history-max 3 -n simple-batch .helm
+helm upgrade --install simple-cronjob --history-max 3 -n example-app ../.helm
