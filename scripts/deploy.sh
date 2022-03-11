175c01af726d2d2c48953acddc8be0f6b3c64109
kanutm <kanutm@NongAct.local>
2022-03-11 14:22:24 +0700

    update : helm value.yaml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,15 @@
+
+
+#Building Image 
 docker build -t asia.gcr.io/poc-innovation-iot/simple-cronjob:v1.0 ..
+
+#Push built Image to container Registry
 docker push asia.gcr.io/poc-innovation-iot/simple-cronjob:v1.0
 
+#Get Creditdential set to destination project
+# gcloud container clusters get-credentials clustername --zone zone --project project
 gcloud container clusters get-credentials poc-cluster-cmmn --zone asia-southeast1-b --project poc-innovation-iot
+
+#Deploy project using helm file 
+#helm upgrade --install name --history-max 3 -n namespace {helm_location}
 helm upgrade --install simple-cronjob --history-max 3 -n example-app ../.helm
